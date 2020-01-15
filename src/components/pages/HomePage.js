79f1dfef449a492c8e91bb88692518dca9706966
maxPiroddi <mpiroddi@outlook.com>
2020-01-15 12:34:58 +1100

    Fixed export issues

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Form from "./../forms/Form";
+import Results from "./Results";
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to the <i>Relaxation Station</i>!</h1>
+        <h1>
+          Welcome to the <i>Relaxation Station</i>!
+        </h1>
         <div>
           <h3>How have you been feeling today?</h3>
-          <Form />
         </div>
       </div>
-    )
+    );
   }
 }
+
+export default HomePage;
