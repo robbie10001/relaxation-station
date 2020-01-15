3d1efcf1b002121a952f0675cc638ef26c5ab98c
alkex1 <alikentwell@hotmail.com>
2020-01-15 11:59:56 +1100

    added route

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import Homepage from "./pages/HomePage";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
-    return null;
+    return (
+      <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/relaxation_station" component={HomePage} />
+          </Switch>
+          </div>
+          </BrowserRouter>
+    );
   }
 }
+
+export default App;
